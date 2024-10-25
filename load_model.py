585aae4cfaf15a773d60297d9334eb3234cd7ee0
Alan Moreno <alan.dario.moreno@hotmail.com>
2024-10-25 13:04:02 -0300

    Configuracion de GCS, creacion de variables de entorno

---
+++ load_model.py
@@ -0,0 +1,21 @@
+from google.cloud import storage
+import tensorflow as tf
+import os
+from dotenv import load_dotenv
+import os
+
+def load_model():
+    load_dotenv()
+
+    storage_client = storage.Client()
+
+        # Accede al bucket y descarga el archivo
+    bucket = storage_client.bucket(os.getenv("bucket_name"))
+    blob = bucket.blob(os.getenv("model_name"))
+    blob.download_to_filename(os.getenv("local_model_path"))
+
+        # Carga el modelo descargado en TensorFlow
+    model = tf.keras.models.load_model(os.getenv("local_model_path"))
+    print("Modelo cargado correctamente.")
+
+    return model
